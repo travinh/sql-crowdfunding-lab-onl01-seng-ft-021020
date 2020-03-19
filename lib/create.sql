de86c2fbcf511d5e8e76897c026713d6b85fed5a
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 03:05:46 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,18 @@
 CREATE TABLE projects(
+id INTEGER PRIMARY KEY,
 title TEXT,
 category TEXT,
 funding_goal INTEGER,
-start
-)
+start_date DATE,
+end_date DATE
+);
+
+CREATE TABLE users(
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+);
+
+CREATE TABLE pledges(
+
+);
