9fdc8a75bb655dfec277cb68f3e5fe00de0888b3
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 04:01:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,8 +46,9 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, pledges.amount
     FROM projects
     JOIN pledges
-    on project_id = pledges.user_id
+    on project.id = pledges.user_id
     WHERE projects.category = \"music\";"
+    
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
