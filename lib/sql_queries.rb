4aa1588e17d483281f853f9d8150f0fcb0ccc339
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 03:39:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   "SELECT projects.title, SUM(pledges.amount) FROM projects
     INNER JOIN pledges
     on projects.id = pledges.id
-    GROUP BY ;"
+    GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
