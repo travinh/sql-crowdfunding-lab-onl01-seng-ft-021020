1aecf8bde0f77be3e7a090bf0a554bf58745ee39
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 03:41:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,11 +7,12 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  # "SELECT projects.title, SUM(pledges.amount) 
-  #   FROM projects
-  #   INNER JOIN pledges
-  #   on projects.id = pledges.id
-  #   GROUP BY projects.title;"
+  "SELECT projects.title, SUM(pledges.amount) 
+    FROM projects
+    INNER JOIN pledges
+    on projects.id = pledges.id
+    GROUP BY projects.title
+  ORDER BY projects.title;"
   "SELECT projects.title, SUM(pledges.amount)
   FROM projects
   INNER JOIN pledges
